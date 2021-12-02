f3b36c437e12af10392cee8e3a67e3d9961b7cbd
MilcaC <milca.ellen.cm@gmail.com>
2021-12-02 02:28:52 +0000

    Teste Soma

--- calc-api/src/main/testSoma.java
+++ calc-api/src/main/testSoma.java
@@ -1,17 +1,7 @@
-package calculadoraapi;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-
-public class CalculadoraTest 
+public class TestCalculadora 
 {
     
-    public CalculadoraTest() 
+    public TestCalculadora() 
     {
 
     }
